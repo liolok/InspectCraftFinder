5923781de7df542dfcefc690b34f7e74fa2b151e
liolok <i@liolok.com>
2025-04-06 02:07:11 +0800

    refactor

--- modinfo.lua
+++ modinfo.lua
@@ -1,15 +1,14 @@
-name = "Inspect Craft Finder"
-author = "Sinny Deas"
-version = "1.7"
-description = "Description \n"..
-"- Show possible crafting recipes, in the crafting menu, when inspecting something.\n"..
-"- Item can be on the ground or in the inventory.\n\n"..
-"Keyboard/Mouse and Gamepad \n"..
-"- Both supported.\n\n"..
-"v1.7 \n"
+name = 'Inspect Craft Finder'
+author = 'Sinny Deas'
+version = '1.7'
+description = 'Description \n'
+  .. '- Show possible crafting recipes, in the crafting menu, when inspecting something.\n'
+  .. '- Item can be on the ground or in the inventory.\n\n'
+  .. 'Keyboard/Mouse and Gamepad \n'
+  .. '- Both supported.\n\n'
+  .. 'v1.7 \n'
 
-
-forumthread = ""
+forumthread = ''
 api_version = 10
 
 dst_compatible = true
@@ -21,7 +20,7 @@ forge_compatible = false
 client_only_mod = true
 all_clients_require_mod = false
 
-server_filter_tags = {"Inspect", "Finder", "Inspect Craft Finder", "InspectCraftFinder"}
+server_filter_tags = { 'Inspect', 'Finder', 'Inspect Craft Finder', 'InspectCraftFinder' }
 
-icon_atlas = "icon.xml"
-icon = "icon.tex"
+icon_atlas = 'icon.xml'
+icon = 'icon.tex'
